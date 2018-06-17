899876514adf3393be0f60dc6a6d928daeee2ffe
@devpg <andre.neubauer@googlemail.com>
2018-06-17 14:36:19 +0200

    Revert "Install app store updates automatically"
    This reverts commit f71f9eecca48c636275009e30577b89f4e5a4e77.

--- install.sh
+++ install.sh
@@ -11,18 +11,15 @@ chmod +x install.sh
 # link dotfiles
 ln -s dotfiles/.profile ~/.profile
 
-# install brew and apps (incl. app store)
+# brew stuff
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-cd config
 brew bundle
-mas signin andre.neubauer@googlemail.com
-mas upgrade
 
 # ruby
 # rbenv install 2.2.5
 # rbenv global 2.2.5
 
-# gem install bundler
+gem install bundler
 
 echo "******************** Done ********************"
 echo "Don't forget to configure SSH properly with key and config"
